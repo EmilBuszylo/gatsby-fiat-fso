e625f6df7a4f8c537154fa1fefd981149a1eeba9
EmilBuszylo <emil.interesy@gmail.com>
2018-07-20 09:39:55 +0200

    set fake front page for netlify

--- src/templates/front-page.js
+++ src/templates/front-page.js
@@ -1,59 +1,12 @@
 import React from 'react'
 
-import Carousel from 'nuka-carousel';
-
-import Content from '../components/Content';
-import Helmet from 'react-helmet'
-
 export default class FrontPage extends React.Component {
 
   render() {
-    const { markdownRemark } = this.props.data
 
     return (
-      <div id="front-page">
-        <Helmet
-          meta={[
-            { name: 'title', content: markdownRemark.frontmatter.title },
-            { name: 'description', content: markdownRemark.frontmatter.description },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
-        />
-        <Carousel autoplay={true} wrapAround={true} autoplayInterval={4000}>
-          <img src={markdownRemark.frontmatter.slider_img.image1.image} />
-          <img src={markdownRemark.frontmatter.slider_img.image2.image} />
-          <img src={markdownRemark.frontmatter.slider_img.image3.image} />
-        </Carousel>
-        <h1>{markdownRemark.frontmatter.title}</h1>
-        <Content content={markdownRemark.html} />
+      <div id="fake-front-page">
       </div>
     )
   }
 }
-
-// export const frontPage = graphql`
-// query FrontPage($path: String!) {
-//     markdownRemark(frontmatter: { path: { eq: $path } }) {
-//       html
-//       frontmatter {
-//         title
-//         path
-//         description
-//         slider_img {
-//           image1 {
-//             alt
-//             image
-//           }
-//           image2 {
-//             alt
-//             image
-//           }
-//           image3 {
-//             alt
-//             image
-//           }
-//         }
-//       }
-//     }
-//   }
-// `;

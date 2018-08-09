66883ca970dfb692abca0162384ba1b05fc15ed0
emilbuszylo <emil.interesy@gmail.com>
2018-08-09 12:29:36 +0200

    changed contact page style

--- src/templates/kontakt.js
+++ src/templates/kontakt.js
@@ -18,8 +18,8 @@ export default class Kontakt extends React.Component {
           ]}
         />
         <h1 className="page-title">{markdownRemark.frontmatter.title}</h1>
-        <Content content={markdownRemark.html} />
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2440.6987729331354!2d20.934358115638847!3d52.285170461358284!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471eca2e21c359b3%3A0xe65aa706839aee48!2sDantego%2C+Warszawa!5e0!3m2!1spl!2spl!4v1532931132923" className="contact-map" frameBorder="0" allowFullScreen></iframe>
+        <Content content={markdownRemark.html} />
       </div>
     )
   }
